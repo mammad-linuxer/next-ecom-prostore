043cf13be265264b234e49bc8441634b05f5aee8
Mammad Linuxer <mammad.linuxer@gmail.com>
2026-06-30 20:07:35 +0330

    install lucide-react & add footer/header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,7 @@
+import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
+// import { Inter } from "next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -5,8 +9,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen flex-col">
-      <main className="flex-1 wrapper">{children}</main>
-    </div>
+    /*<html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}}`>*/
+        <div className="flex h-screen flex-col">
+          <Header />
+          <main className="flex-1 wrapper">{children}</main>
+          <Footer />
+        </div>
+      /*</body>
+    </html>*/
   );
 }
