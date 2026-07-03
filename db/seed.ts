40ec51efb019ed73fd38d1bb3fd81040cf9f25de
Mammad Linuxer <mammad.linuxer@gmail.com>
2026-07-04 02:51:47 +0330

    modify the seed.ts file

--- db/seed.ts
+++ db/seed.ts
@@ -1,11 +1,12 @@
 // import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
 import sampleData from "./sample-data";
+import { prisma } from "@/db/prisma";
 
 async function main() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-  const prisma = new PrismaClient({ adapter });
+  //   const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+  //   const prisma = new PrismaClient({ adapter });
   await prisma.product.deleteMany();
   await prisma.product.createMany({ data: sampleData.products });
 
