b4c7282f96d51356fe5a254c1874fd770ab7d60f
Mammad Linuxer <mammad.linuxer@gmail.com>
2026-07-04 02:40:20 +0330

    serverless env config

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,14 +1,15 @@
 "use server";
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma";
 
 // Get the Latest Products
 export async function getLatestProducts() {
-  const connectionString = process.env.DATABASE_URL;
-  const adapter = new PrismaPg({ connectionString: connectionString });
-  const prisma = new PrismaClient({ adapter });
+  // const connectionString = process.env.DATABASE_URL;
+  // const adapter = new PrismaPg({ connectionString: connectionString });
+  // const prisma = new PrismaClient({ adapter });
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
