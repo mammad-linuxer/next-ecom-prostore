deac7536ce34458e3952b5e75e6d82e92b9ca629
Mammad Linuxer <mammad.linuxer@gmail.com>
2026-07-07 16:42:27 +0330

    seed user data to db

--- db/seed.ts
+++ db/seed.ts
@@ -7,8 +7,17 @@ import { prisma } from "@/db/prisma";
 async function main() {
   //   const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
   //   const prisma = new PrismaClient({ adapter });
+
+  /*Deleting all the data in the database*/
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
+  /*create data from sampleData*/
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database Seeded Successfully");
 }
