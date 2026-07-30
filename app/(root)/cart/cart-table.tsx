16ef844bffd47cd5ca2af89c16856b13573eb758
Mammad Linuxer <mammad.linuxer@gmail.com>
2026-07-30 19:09:17 +0330

    add shadcn-table

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -6,8 +6,19 @@ import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
 import { toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { Cart } from "@/types";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
+  const [isPending, startTransition] = useTransition();
   return (
     <>
       <h1 className="py-4 h2-bold">CartTable</h1>
@@ -17,7 +28,91 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
         </div>
       ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
-          <div className=" overflow-x-auto md:col-span-3 ">Cart is Empty</div>
+          <div className=" overflow-x-auto md:col-span-3 ">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2">
+                      <Button
+                        disabled={isPending}
+                        variant={"outline"}
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await removeItemFromCart(
+                              item.productId,
+                            );
+                            if (!res.success) {
+                              toast.error(res.message, { className: "bg-red" });
+                            } else {
+                              toast.success(res.message, {
+                                className: "bg-green",
+                              });
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="animate-spin w-4 h-4" />
+                        ) : (
+                          <Minus />
+                        )}
+                      </Button>
+                      <span>{item.qty}</span>
+                      <Button
+                        disabled={isPending}
+                        variant={"outline"}
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await addItemToCart(item);
+                            if (!res.success) {
+                              toast.error(res.message);
+                            } else {
+                              toast.success(res.message, {
+                                className: "bg-grey-400",
+                              });
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="animate-spin h-4 w-4" />
+                        ) : (
+                          <Plus />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">
+                      ${item.price /* * item.qty */}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       )}
     </>
