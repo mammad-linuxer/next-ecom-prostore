0c1c94cae87f4da67a8e8fec1106ab303dcab569
Mammad Linuxer <mammad.linuxer@gmail.com>
2026-08-01 01:47:16 +0330

    shipping-address-form & update address

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -5,6 +5,7 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
+import ShippingAddressForm from "./shipping-address-form";
 
 export const metadata: Metadata = {
   title: "Shipping Address",
@@ -22,7 +23,11 @@ const ShippingAddressPage = async () => {
     // throw new Error("User Not Found!");
   }
   const user = await getUserById(userId);
-  return <>Shipping Address Page</>;
+  return (
+    <>
+      <ShippingAddressForm address={user.address as ShippingAddress} />
+    </>
+  );
 };
 
 export default ShippingAddressPage;
---
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -0,0 +1,188 @@
+"use client";
+import React, { useTransition } from "react";
+
+import {
+  SubmitHandler,
+  useForm,
+  // ControllerRenderProps,
+  Controller,
+} from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { ShippingAddress } from "@/types";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { shippingAddressSchema } from "@/lib/validator";
+import { shippingAddressDefaultValues } from "@/lib/constants";
+import { toast } from "sonner";
+import { updateUserAddress } from "@/lib/actions/user.actions";
+//import CheckoutSteps from "@/components/shared/checkout-steps";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader } from "lucide-react";
+
+const ShippingAddressForm = ({
+  address,
+}: {
+  address: ShippingAddress | null;
+}) => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof shippingAddressSchema>>({
+    resolver: zodResolver(shippingAddressSchema),
+    defaultValues: address || shippingAddressDefaultValues,
+  });
+
+  const [isPending, startTransition] = useTransition();
+
+  const onSubmit: SubmitHandler<z.infer<typeof shippingAddressSchema>> = async (
+    values,
+  ) => {
+    startTransition(async () => {
+      const res = await updateUserAddress(values);
+
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
+      // THIS LINE IS ADDED by me
+      toast.success(res.message);
+      // THIS LINE IS ADDED by me
+
+      setTimeout(() => {
+        router.push("/payment-methods");
+      }, 1500);
+    });
+  };
+  return (
+    <>
+      <div className="max-w-md mx-auto space-y-4">
+        <h1 className="h2-bold mt-4">Shipping Address</h1>
+        <p className="text-sm text-muted-foreground">
+          Please enter the address that you want to ship to
+        </p>
+        <form
+          method="post"
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4"
+        >
+          <FieldGroup>
+            <div className="flex flex-col gap-5 md:flex-row">
+              <Controller
+                name="fullName"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field className="w-full" data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="fullName">Full Name</FieldLabel>
+                    <Input
+                      {...field}
+                      id="fullName"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter Full Name"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </div>
+            <Controller
+              name="streetAddress"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field className="w-full" data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="streetAddress">Address</FieldLabel>
+                  <Input
+                    {...field}
+                    id="streetAddress"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter Address"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <div className="flex flex-col gap-5 md:flex-row">
+              <Controller
+                name="city"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field className="w-full" data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="city">City</FieldLabel>
+                    <Input
+                      {...field}
+                      id="city"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter city"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <Controller
+                name="country"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field className="w-full" data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="country">Country</FieldLabel>
+                    <Input
+                      {...field}
+                      id="country"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter country"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <Controller
+                name="PostalCode"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field className="w-full" data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="postalCode">Postal Code</FieldLabel>
+                    <Input
+                      {...field}
+                      id="postalCode"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter postal code"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </div>
+            <div className="flex gap-2">
+              <Button type="submit" disabled={isPending}>
+                {isPending ? (
+                  <Loader className="animate-spin" />
+                ) : (
+                  <ArrowRight className="w-4 h-4" />
+                )}
+                Continue
+              </Button>
+            </div>
+          </FieldGroup>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ShippingAddressForm;
