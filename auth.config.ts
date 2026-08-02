9f79c25d7777907cbe35700b10aa0c94daa7201b
Mammad Linuxer <mammad.linuxer@gmail.com>
2026-08-02 17:13:10 +0330

    protect auth paths

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,7 @@ import { NextAuthConfig } from "next-auth";
 import { NextResponse } from "next/server";
 
 export const authConfig = {
- // trustHost: true,
+  // trustHost: true,
   pages: {
     signIn: "/sign-in",
     error: "sign-in",
@@ -11,6 +11,23 @@ export const authConfig = {
   callbacks: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     authorized({ request, auth }: any) {
+      // Array of regex patterns of protected paths
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and on a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       // Check for cart cookie
       if (!request.cookies.get("sessionCartId")) {
         const sessionCartId = crypto.randomUUID();
