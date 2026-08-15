036711ab44aa3b115b04908a1b73e6451a297ea0
Mammad Linuxer <mammad.linuxer@gmail.com>
2026-08-15 23:13:59 +0330

    admin orders page

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { auth } from "@/auth";
+import { getAllOrders } from "@/lib/actions/order.actions";
+import { Metadata } from "next";
+import { requireAdmin } from "@/lib/auth-guard";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Pagination from "@/components/shared/pagination";
+
+export const metadata: Metadata = {
+  title: "Admin Orders",
+};
+
+const OrdersPage = async (props: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  await requireAdmin();
+
+  const { page = "1" } = await props.searchParams;
+
+  const session = await auth();
+
+  const orders = await getAllOrders({ page: Number(page) });
+
+  return (
+    <div className="space-y-2">
+      <h2 className="h2-bold">Orders</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>OWNER</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>TOTAL</TableHead>
+              <TableHead>PAID</TableHead>
+              <TableHead>DELIVERED</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>{order.user.name}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>
+                  {formatCurrency(order.totalPrice.toNumber())}
+                </TableCell>
+                <TableCell>
+                  {order.paidAt
+                    ? formatDateTime(order.paidAt).dateTime
+                    : "Not Paid!"}
+                </TableCell>
+                <TableCell>
+                  {order.deliveredAt
+                    ? formatDateTime(order.deliveredAt).dateTime
+                    : "Not Delivered!"}
+                </TableCell>
+                <TableCell>
+                  <Button asChild variant={"outline"}>
+                    <Link href={`/order/${order.id}`}>Details</Link>
+                  </Button>
+                  {/* DELETEf */}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          {orders.totalPages > 1 && (
+            <Pagination
+              page={Number(page) || 1}
+              totalPages={orders.totalPages}
+            />
+          )}
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default OrdersPage;
